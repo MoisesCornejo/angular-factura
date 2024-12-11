c922303387de0011b46900a1d17918c59ad465f3
MoisesCornejo <moises.cornejo@inacapmail.cl>
2024-12-11 12:04:51 -0300

    Se agrego tailwind css y daisyUI para componentes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 import {InvoiceComponent} from './app/components/invoice/invoice.component';
 
 // cambiamos el componente principal al InvoiceComponent
