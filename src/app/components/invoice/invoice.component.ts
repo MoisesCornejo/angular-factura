720e2696c74a8d1af13fa289f78265f9b95711ae
MoisesCornejo <moises.cornejo@inacapmail.cl>
2024-12-11 11:07:52 -0300

    Se creo un service para retornar la data de la factura, se realizo inyección de dependencias del invoiceService desde el invoice.component, se implemento OnInit para pasar datos del service al html de invoice y en el html se maquetaron los datos de la factura y se recorrieron los items de la factura mediante un @for

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InvoiceService} from '../../services/invoice.service';
+import {Invoice} from '../../models/invoice';
 
 @Component({
   selector: 'app-invoice',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit { // implementamos OnInit
+
+  // creamos variable de tipo factura
+  invoice!: Invoice;
+
+  // Definimos constructor para inyectar el service con los datos
+  // el atributo que se pasa es para realizar inyección de dependencias es lo mismo que hacerlo afuera
+  // solo que ahora es un atributo de la clase
+  constructor(private service: InvoiceService) {}
+
+  ngOnInit(): void {
+    // pasamos el service a la variable para utilizarlo en la plantilla html
+    this.invoice = this.service.getInvoice();
+  }
 
 }
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -5,7 +5,7 @@ import {invoiceData} from  '../data/invoice.data'
 @Injectable({
   providedIn: 'root'
 })
-export class ItemService {
+export class InvoiceService {
 
   // importamos datos al service, ademas al indicar que los datos son de tipo invoice
   // nos indicara los errores que existan en la data
