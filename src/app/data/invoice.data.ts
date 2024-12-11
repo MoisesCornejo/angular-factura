1a34cc506b7b0a53480c98d5c7bfb49aaa7f6960
MoisesCornejo <moises.cornejo@inacapmail.cl>
2024-12-11 10:34:45 -0300

    Creamos datos ficticios para los datos de una factura y creamos un ItemService para devolver los datos de la factura

---
+++ src/app/data/invoice.data.ts
@@ -0,0 +1,41 @@
+import {Invoice} from '../models/invoice';
+
+// indicamos que los datos son de tipo invoice!
+export const invoiceData: Invoice = {
+  id:1,
+  name: 'Componentes de pc',
+  client: {
+    name: 'Moises',
+    lastname: 'Doe',
+    address: {
+      country: 'USA',
+      city: 'Los Angeles',
+      street: 'One Street',
+      number: 15,
+    },
+  },
+  company: {
+    name:'New Age',
+    fiscalNumber: 3221454,
+  },
+  items: [
+    {
+      id: 1,
+      product: 'CPU intel I9',
+      price: 599,
+      quantity: 1,
+    },
+    {
+      id: 2,
+      product: 'Corsair Teclado 80%',
+      price: 399,
+      quantity: 2,
+    },
+    {
+      id: 1,
+      product: 'Monitor Asus',
+      price: 699,
+      quantity: 3,
+    }
+  ]
+}
---
+++ src/app/services/item.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Invoice} from '../models/invoice';
+import {invoiceData} from  '../data/invoice.data'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  // importamos datos al service, ademas al indicar que los datos son de tipo invoice
+  // nos indicara los errores que existan en la data
+  private invoice: Invoice = invoiceData;
+
+  constructor() { }
+
+  getInvoice(): Invoice {
+    return this.invoice;
+  }
+}
